a28e81fe42f4148160ab6b5869f435d44b3ef904
Mahdi Shams <seyedmah@buffalo.edu>
2015-02-25 16:24:35 -0500

    Re-start

---
+++ src/daedalus_control/scripts/move_leg.sh
@@ -0,0 +1,98 @@
+#!/bin/bash 
+rostopic pub -1 /rrbot/joint2_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint3_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint4_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint5_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint1_position_controller/command std_msgs/Float64 "data: 0"
+
+rostopic pub -1 /rrbot/joint1_position_controller/command std_msgs/Float64 "data: 2"
+rostopic pub -1 /rrbot/joint2_position_controller/command std_msgs/Float64 "data: 0.5"
+rostopic pub -1 /rrbot/joint3_position_controller/command std_msgs/Float64 "data: 0.5"
+rostopic pub -1 /rrbot/joint4_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint2_position_controller/command std_msgs/Float64 "data: -0.5"
+
+rostopic pub -1 /rrbot/joint1_position_controller/command std_msgs/Float64 "data: -0.7"
+
+rostopic pub -1 /rrbot/joint2_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint3_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint4_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint5_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint1_position_controller/command std_msgs/Float64 "data: 0"
+
+rostopic pub -1 /rrbot/joint1_position_controller/command std_msgs/Float64 "data: 2"
+rostopic pub -1 /rrbot/joint2_position_controller/command std_msgs/Float64 "data: 0.5"
+rostopic pub -1 /rrbot/joint3_position_controller/command std_msgs/Float64 "data: 0.5"
+rostopic pub -1 /rrbot/joint4_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint2_position_controller/command std_msgs/Float64 "data: -0.5"
+
+rostopic pub -1 /rrbot/joint1_position_controller/command std_msgs/Float64 "data: -0.7"
+
+rostopic pub -1 /rrbot/joint2_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint3_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint4_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint5_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint1_position_controller/command std_msgs/Float64 "data: 0"
+
+rostopic pub -1 /rrbot/joint1_position_controller/command std_msgs/Float64 "data: 2"
+rostopic pub -1 /rrbot/joint2_position_controller/command std_msgs/Float64 "data: 0.5"
+rostopic pub -1 /rrbot/joint3_position_controller/command std_msgs/Float64 "data: 0.5"
+rostopic pub -1 /rrbot/joint4_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint2_position_controller/command std_msgs/Float64 "data: -0.5"
+
+rostopic pub -1 /rrbot/joint1_position_controller/command std_msgs/Float64 "data: -0.7"
+
+rostopic pub -1 /rrbot/joint2_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint3_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint4_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint5_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint1_position_controller/command std_msgs/Float64 "data: 0"
+
+rostopic pub -1 /rrbot/joint1_position_controller/command std_msgs/Float64 "data: 2"
+rostopic pub -1 /rrbot/joint2_position_controller/command std_msgs/Float64 "data: 0.5"
+rostopic pub -1 /rrbot/joint3_position_controller/command std_msgs/Float64 "data: 0.5"
+rostopic pub -1 /rrbot/joint4_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint2_position_controller/command std_msgs/Float64 "data: -0.5"
+
+rostopic pub -1 /rrbot/joint1_position_controller/command std_msgs/Float64 "data: -0.7"
+
+rostopic pub -1 /rrbot/joint2_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint3_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint4_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint5_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint1_position_controller/command std_msgs/Float64 "data: 0"
+
+rostopic pub -1 /rrbot/joint1_position_controller/command std_msgs/Float64 "data: 2"
+rostopic pub -1 /rrbot/joint2_position_controller/command std_msgs/Float64 "data: 0.5"
+rostopic pub -1 /rrbot/joint3_position_controller/command std_msgs/Float64 "data: 0.5"
+rostopic pub -1 /rrbot/joint4_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint2_position_controller/command std_msgs/Float64 "data: -0.5"
+
+rostopic pub -1 /rrbot/joint1_position_controller/command std_msgs/Float64 "data: -0.7"
+
+rostopic pub -1 /rrbot/joint2_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint3_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint4_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint5_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint1_position_controller/command std_msgs/Float64 "data: 0"
+
+rostopic pub -1 /rrbot/joint1_position_controller/command std_msgs/Float64 "data: 2"
+rostopic pub -1 /rrbot/joint2_position_controller/command std_msgs/Float64 "data: 0.5"
+rostopic pub -1 /rrbot/joint3_position_controller/command std_msgs/Float64 "data: 0.5"
+rostopic pub -1 /rrbot/joint4_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint2_position_controller/command std_msgs/Float64 "data: -0.5"
+
+rostopic pub -1 /rrbot/joint1_position_controller/command std_msgs/Float64 "data: -0.7"
+
+rostopic pub -1 /rrbot/joint2_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint3_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint4_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint5_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint1_position_controller/command std_msgs/Float64 "data: 0"
+
+rostopic pub -1 /rrbot/joint1_position_controller/command std_msgs/Float64 "data: 2"
+rostopic pub -1 /rrbot/joint2_position_controller/command std_msgs/Float64 "data: 0.5"
+rostopic pub -1 /rrbot/joint3_position_controller/command std_msgs/Float64 "data: 0.5"
+rostopic pub -1 /rrbot/joint4_position_controller/command std_msgs/Float64 "data: 0"
+rostopic pub -1 /rrbot/joint2_position_controller/command std_msgs/Float64 "data: -0.5"
+
+rostopic pub -1 /rrbot/joint1_position_controller/command std_msgs/Float64 "data: -0.7"
